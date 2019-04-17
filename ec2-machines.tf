b3a9ab8aa0bff364282cbf1f392e51598cdd59c8
ffdavis <33498961+ffdavis@users.noreply.github.com>
2019-04-16 22:05:08 -0400

    Update ec2-machines.tf
    Giuseppe,
    In my case, I had to add those 2 lines between -----correction----- in order to GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' and CREATE DATABASE test to get the right result in the html/php calldb.php page. 
    Thanks!

--- ec2-machines.tf
+++ ec2-machines.tf
@@ -34,15 +34,19 @@ resource "aws_instance" "database" {
   tags {
         Name = "database"
   }
-  user_data = <<HEREDOC
+  user_data = <<-HEREDOC
   #!/bin/bash
   sleep 180
-  yum update -y
-  yum install -y mysql55-server
-  service mysqld start
+  sudo yum update -y
+  sudo yum install -y mysql55-server
+  sudo service mysqld start
   /usr/bin/mysqladmin -u root password 'secret'
   mysql -u root -psecret -e "create user 'root'@'%' identified by 'secret';" mysql
+  #---- start-correction ----
+  mysql -u root -psecret -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';" mysql
+  mysql -u root -psecret -e 'CREATE DATABASE test;'
+  #---- end-correction ----
   mysql -u root -psecret -e 'CREATE TABLE mytable (mycol varchar(255));' test
   mysql -u root -psecret -e "INSERT INTO mytable (mycol) values ('linuxacademythebest') ;" test
-HEREDOC
-}
+  HEREDOC
+  }
